9f09008155774944a188f96d8ccd056df4759321
/配置姓名 <1299898126@qq.com>
2020-11-18 14:16:28 +0800

    1

---
+++ 13  Vue/Vue/02_基础/2-代码/js/time.js
@@ -0,0 +1,17 @@
+/** 
+ * 对日期进行格式化， 
+ * @param date 要格式化的日期 
+ * @param format 进行格式化的模式字符串
+ *     支持的模式字母有： 
+ *     y:年, 
+ *     M:年中的月份(1-12), 
+ *     d:月份中的天(1-31), 
+ *     h:小时(0-23), 
+ *     m:分(0-59), 
+ *     s:秒(0-59), 
+ *     S:毫秒(0-999),
+ *     q:季度(1-4)
+ * @return String
+ * @author yanis.wang
+ * @see http://yaniswang.com/frontend/2013/02/16/dateformat-performance/
+ */
