d9107a41dc65cecd753d9aaaaa8d9dadf6116463
/配置姓名 <1299898126@qq.com>
2020-11-25 18:20:21 +0800

    1

--- Vue 课件/Vue/06_前端工程化/Day_006/webpack.config.js
+++ Vue 课件/Vue/06_前端工程化/Day_006/webpack.config.js
@@ -1,13 +1,8 @@
-const path = require('path')
+const path = require('path') // 导入 node.js 中专门操作路径的模块
 module.exports = {
-	mode: 'development',
-	// 设置入口文件路径
-	entry: path.join(__dirname, './src/index.js'),
-	// 设置出口文件
+	entry: path.join(__dirname, './src/index.js'), // 打包入口文件的路径
 	output: {
-		// 设置路径
-		path: path.join(__dirname, './dist'),
-		// 设置文件名
-		filename: 'index.js',
+		path: path.join(__dirname, './dist'), // 输出文件的存放路径
+		filename: 'bundle.js', // 输出文件的名称
 	},
 }
